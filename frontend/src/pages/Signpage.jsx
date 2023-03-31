3fa5c050867f67d1bc31805de9736f9d6bf541b8
himanshi4902 <sharmahimanshi0094@gmail.com>
2023-03-31 23:37:30 +0530

    sign page

---
+++ frontend/src/pages/Signpage.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import "./Loginform.css";
+import Card from "../Card/Card";
+import { FaGoogle } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaTwitter} from 'react-icons/fa';
+
+
+const Loginform = () => {
+  return <Card>
+    
+    <h1 className='title'>Sign In</h1>
+    <hr className="horizontal-line" />
+    <form>
+        <div className='input_Container'>
+            <input type="text" placeholder='username' />
+            <input type="text" placeholder='email'/>
+            <input type="password" placeholder='password' />
+        </div>
+        <input type='submit' value="Log In" className='login_button' />
+    </form>
+    <div className='link_container'>
+        <a href="" className='small'>
+           Forgot password?
+        </a>
+    </div>
+    <div className='icons'><FaGoogle style={{ color: 'white', fontSize: '24px' }} />
+    <FaGithub style={{ color: 'white', fontSize: '24px' }} />
+    <FaTwitter style={{ color: 'white', fontSize: '24px' }} />
+    </div>
+
+
+
+  </Card>;
+}
+
+export default Loginform
